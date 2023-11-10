0d0267fedf544365c77cf4464a0c2e59813e7cfe
introtoxicXXl <minhajulabedin648@gmail.com>
2023-11-11 00:20:55 +0600

    handle search done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,14 @@
+import PropTypes from 'prop-types';
 import './Banner.css';
-const Banner = () => {
+
+const Banner = ({ handleSearch }) => {
     return (
         <div className='banner z-10'>
             <h1 className='lg:text-5xl md:text-4xl font-bold text-black'>I Grow By Helping People In Need</h1>
             <div className='lg:w-2/4 w-full md:w-2/4 mx-auto mt-5'>
-                <form className="flex items-center">
+                <form onSubmit={handleSearch} className="flex items-center">
                     <div className="relative w-full mr-1">
-                        <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full pl-3 p-2.5" placeholder="Search Here"/>
+                        <input name='input' type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full pl-3 p-2.5" placeholder="Search Here" />
                     </div>
                     <button type="submit" className="btn bg-red-600 text-white hover:bg-red-700">
                         Search
@@ -15,6 +17,9 @@ const Banner = () => {
             </div>
         </div>
     );
+};
+Banner.propTypes = {
+    handleSearch: PropTypes.func.isRequired
 };
 
 export default Banner;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,20 +5,48 @@ import Donet from "../Donet/Donet";
 
 const Home = () => {
     const [donets, setDonets] = useState([]);
+    const [displayItem, setDisplayItem] = useState([])
+
     useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
-            .then(data => setDonets(data))
+            .then(data => {
+                setDonets(data)
+                setDisplayItem(data);
+            })
 
     }, [])
+    const handleSearch = e => {
+        const inputText = e.target.input.value;
+        if (inputText.toLowerCase() === 'education') {
+            const educationItem = donets.filter(donat => donat.category === 'Education')
+            setDisplayItem(educationItem);
+        }else if (inputText.toLowerCase() === 'food') {
+            const educationItem = donets.filter(donat => donat.category === 'Food')
+            setDisplayItem(educationItem);
+        }else if (inputText.toLowerCase() === 'health') {
+            const educationItem = donets.filter(donat => donat.category === 'Health')
+            setDisplayItem(educationItem);
+        }else if (inputText.toLowerCase() === 'clothing') {
+            const educationItem = donets.filter(donat => donat.category === 'Clothing')
+            setDisplayItem(educationItem);
+        }else if (inputText.toLowerCase() === 'all') {
+            setDisplayItem(donets);
+        }
+
+
+
+
+        e.preventDefault()
+    }
     return (
         <div>
             <div className="flex justify-center items-center home">
-                <Banner></Banner>
+                <Banner handleSearch={handleSearch}></Banner>
             </div>
             <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-3 mt-28">
                 {
-                    donets.map(donet => <Donet
+                    displayItem.map(donet => <Donet
                         key={donet.id}
                         donet={donet}
                     ></Donet>)
